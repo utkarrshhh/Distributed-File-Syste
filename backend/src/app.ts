4fac7c7c329f2a1085fdee1cfe6b2ccbb5f06222
Utkarsh Shukla <utkarrshhh@Utkarshs-MacBook-Air.local>
2026-09-19 18:07:26 +0530

    fixes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://distributed-file-syste.vercel.app/",
+      // "https://distributed-file-syste.vercel.app/",
+      "https://filesystem.utkarshshukla.dev/",
     ],
   })
 );
