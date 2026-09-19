415c080392a757812f211dc388f6dce5437d3069
Utkarsh Shukla <utkarrshhh@Utkarshs-MacBook-Air.local>
2026-09-19 15:57:27 +0530

    minor changes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,8 +2,8 @@ import "dotenv/config"
 import app from "./app.ts";
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
+});
