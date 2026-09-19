d85fb9d4cd2028e9d89e2d0185e618589ba0bc61
Utkarsh Shukla <utkarrshhh@Utkarshs-MacBook-Air.local>
2026-09-19 16:41:40 +0530

    all done

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,8 +5,15 @@ const cors = require('cors'); // Import cors
 
 const app = express();
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://distributed-file-syste.vercel.app/",
+    ],
+  })
+);
 
-app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/files", filesRoutes);
