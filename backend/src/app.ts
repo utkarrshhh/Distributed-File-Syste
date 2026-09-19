54e0f8f194407ecc2c0c8f3504d4905bc5f60e81
Utkarsh Shukla <utkarrshhh@Utkarshs-MacBook-Air.local>
2026-09-19 21:48:33 +0530

    checking the cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://distributed-file-syste.vercel.app/",
+      // "https://distributed-file-syste.vercel.app/",
       "https://filesystem.utkarshshukla.dev/",
     ],
   })
